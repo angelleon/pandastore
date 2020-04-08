08156b7a85e0e3a9535801cf62f34bc331365583
Angel Leon <luianglenlop@gmail.com>
2020-04-08 04:49:27 -0500

    changed db structure

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,6 +1,8 @@
-INSERT INTO User (username, passwd) 
+INSERT INTO User (email, passwd, givenName, surname) 
             VALUES ('panda_store', 
-                    '4675e40a866bba7e9baa16534845c7819184df8c831ea89596e10f1fc63fe93a');
-INSERT INTO User (username, passwd) 
+                    '4675e40a866bba7e9baa16534845c7819184df8c831ea89596e10f1fc63fe93a',
+                    "panda", "admin");
+INSERT INTO User (email, passwd, givenName, surname) 
             VALUES ('luianglenlop@gmail.com', 
-                    '4675e40a866bba7e9baa16534845c7819184df8c831ea89596e10f1fc63fe93a');
+                    '4675e40a866bba7e9baa16534845c7819184df8c831ea89596e10f1fc63fe93a',
+                    "angel", "leon");
