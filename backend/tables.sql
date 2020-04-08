0289c82929eb0d1e80732747ca966c977820c831
Angel Leon <luianglenlop@gmail.com>
2020-04-08 04:49:49 -0500

    changed db structure

--- backend/tables.sql
+++ backend/tables.sql
@@ -1,6 +1,23 @@
 DROP TABLE IF EXISTS User;
 CREATE TABLE User(idUser INTEGER AUTO_INCREMENT,
-                  username VARCHAR(32) NOT NULL,
+                  email VARCHAR(32) NOT NULL,
                   passwd VARCHAR(64) NOT NULL,
+                  givenName VARCHAR(100) NOT NULL,
+                  surname VARCHAR(100) NOT NULL,
+                  active BOOLEAN NOT NULL DEFAULT TRUE,
+                  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                  updatedAt TIMESTAMP DEFAULT NULL,
                   CONSTRAINT pkUser PRIMARY KEY (idUser),
                   CONSTRAINT ckPasswdLength CHECK (LENGTH(passwd) = 64));
+
+DROP TABLE IF EXISTS Product;
+CREATE TABLE Product(idProduct INTEGER AUTO_INCREMENT,
+                     name VARCHAR(150),
+                     price DECIMAL(5,2),
+                     photoHash VARCHAR(64) NOT NULL,
+                     description VARCHAR(500),
+                     tags VARCHAR(500),
+                     active BOOLEAN NOT NULL DEFAULT TRUE,
+                     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                     updatedAt TIMESTAMP DEFAULT NULL,
+                     CONSTRAINT pkProduct PRIMARY KEY(idProduct));
