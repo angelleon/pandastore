5bed27f94b1fd6111d5076e7db8f797c48bb7d94
Angel Leon <luianglenlop@gmail.com>
2020-04-08 04:43:32 -0500

    reestrucxtured project

---
+++ services/public/dbService.php
@@ -0,0 +1,29 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../util.php";
+    require_once __DIR__."/../libs/servers/DbServer.php";
+
+    //use PandaStore\Servers\DbServer;
+
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+    use Monolog\Formatter\LineFormatter;
+
+    $formatter = new LineFormatter(null, null, true, true);
+    $handler = new StreamHandler(__DIR__."/../logs/".basename(__FILE__).".log", Logger::DEBUG);
+    $handler->setFormatter($formatter);
+
+    $log = new Logger(basename(__FILE__));
+    $log->pushHandler($handler);
+
+    if (!isset($HTTP_RAW_POST_DATA)) {
+        $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+    }
+    $log->debug($HTTP_RAW_POST_DATA);
+
+    $SERVICE_URL = "$SERVICES_BASE_URL/".basename(__FILE__);
+    nusoap_base::setGlobalDebugLevel(9);
+    $dbService = new DbServer($SERVICE_URL);
+    $dbService->service($HTTP_RAW_POST_DATA);
+    $log->debug($dbService->getDebug());
+
---
+++ services/public/singupService.php
@@ -0,0 +1,19 @@
+<?php
+    //namespace PandaStore\Services;
+    require_once __DIR__."/../util.php";
+    require_once __DIR__."/../libs/servers/DbServer.php";
+    require_once __DIR__."/../libs/servers/SingupServer.php";
+
+    //use PandaStore\Servers\SingupServer;
+    use nusoap_base;
+
+    if (!isset($HTTP_RAW_POST_DATA)) {
+        $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+    }
+
+    //$SERVICE_URL = "$SERVICES_BASE_URL/".basename(__FILE__);
+    nusoap_base::setGlobalDebugLevel(9);
+    $singupService = new SingupServer($SINGUP_SERVICE_URL);
+    $singupService->service($HTTP_RAW_POST_DATA);
+    $log->debug($singupService->getDebug());
+    
